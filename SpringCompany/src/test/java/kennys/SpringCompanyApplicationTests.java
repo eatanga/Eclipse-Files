5904dedf0253fe797b7df3081a4858ac2b67c147
Manny A <54607961+eatanga@users.noreply.github.com>
2020-03-11 16:44:03 -0500

    Create SpringCompanyApplicationTests.java

---
+++ SpringCompany/src/test/java/kennys/SpringCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package kennys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
